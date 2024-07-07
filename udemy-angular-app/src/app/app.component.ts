8654f50d74aa45aae8e340e9b3ecb5465691e292
Wagner Laranjeiras <wlaranjeiras.dev@gmail.com>
2024-07-07 20:07:44 +0200

    work with signals and custom events

--- udemy-angular-app/src/app/app.component.ts
+++ udemy-angular-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,10 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'udemy-angular-app';
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log(`Selected user with id ${id}`)
+  }
+
 }
--- udemy-angular-app/src/app/user/user.component.ts
+++ udemy-angular-app/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
 
 @Component({
   selector: 'app-user',
@@ -12,12 +11,23 @@ const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
 })
 
 export class UserComponent {
-  selectedUser = signal( DUMMY_USERS[randomIndex] );
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar );
+  // Input
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+
+  // Output
+  @Output() select = new EventEmitter();
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  get imagePath() {
+    return `assets/users/${ this.avatar }`;
+  };
 
   onSelectUser() {
-    const randomIndex = Math.floor( Math.random() * DUMMY_USERS.length );
-    this.selectedUser.set( DUMMY_USERS[randomIndex] );
+    this.select.emit(this.id)
   }
 
 
